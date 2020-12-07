0827cc7acecef53acdb03d3a8e17a8f5baddaf00
John Bampton <jbampton@users.noreply.github.com>
2020-12-07 20:56:10 +1000

    Add first lot of files

---
+++ packages/john/index.js
@@ -0,0 +1,17 @@
+const html = `<!doctype html>
+<body>
+  <h1>John Bampton !!</h1>
+  <p></p>
+</body>`
+
+async function handleRequest(request) {
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html;charset=UTF-8",
+    },
+  })
+}
+
+addEventListener("fetch", event => {
+  return event.respondWith(handleRequest(event.request))
+})
